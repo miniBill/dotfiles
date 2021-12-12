0480bee632ef90b004ee9f6037a2a21fd9385355
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2021-12-12 14:16:00 +0100

    Trying to update lamdera

--- programs/lamdera.nix
+++ programs/lamdera.nix
@@ -10,11 +10,13 @@
 stdenv.mkDerivation rec {
   name = "lamdera-${version}";
 
-  version = "0.0.1-alpha12";
+  version = "1.0.1";
 
   src = fetchurl {
     url = "https://static.lamdera.com/bin/linux/lamdera";
     sha256 = "sha256:1qxkfs65cxan6wfgfwcfs8h05sqys3f7km73lm8v36gk5niv4b7q";
+    # url = "https://static.lamdera.com/bin/linux/lamdera-next";
+    # sha256 = "sha256:15bm9ss18nzcdfy8ai3ywki268z81dvpx1ak0z9nc1cifnrw2ajh";
   };
 
   unpackPhase = ":";
