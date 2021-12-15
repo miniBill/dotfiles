d9e604d03bfd46ecddc8fd9bbcfe93445e3474c3
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2021-12-15 15:50:12 +0100

    Move ssh config to repo

--- machine-finland.nix
+++ machine-finland.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 let
   pinned-oldstable = import (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/nixos-20.09.tar.gz) { };
 in
@@ -23,6 +23,10 @@ in
       pinned-oldstable.terraform_0_11 google-cloud-sdk
     ];
   };
+
+  systemd.user.tmpfiles.rules = [
+    "d /home/minibill/.ssh/control 700 minibill users"
+  ];
 
   programs = {
     vscode.userSettings = {
@@ -81,5 +85,48 @@ in
         export ANSIBLE_DISPLAY_OK_HOSTS=no ANSIBLE_DISPLAY_SKIPPED_HOSTS=no ANSIBLE_NOCOLOR=false 
       }
     '';
+
+    ssh = {
+      enable = true;
+      controlMaster = "auto";
+      controlPath = "~/.ssh/control/%r@%h:%p";
+      controlPersist = "10m";
+      matchBlocks = {
+        "10.0.0.*" = lib.hm.dag.entryBefore
+          ["*.rstor.net 209.163.* 10.* ns*.rstorcloud.io"] {
+          user = "minibill";
+        };
+        "rramp-gcp-* 10.128.0.*" = lib.hm.dag.entryBefore
+          ["*.rstor.net 209.163.* 10.* ns*.rstorcloud.io"] {
+          user = "minibill";
+          # pubkeyAuthentication = "yes";
+          proxyJump = "storage-ops-usc.packetfabric.net"; # 10.4.2.21
+        };
+        "*.rstor.net 209.163.* 10.* ns*.rstorcloud.io" = {
+          user = "ltaglialegne";
+          proxyJump = "storage-ops-usc.packetfabric.net";
+        };
+        "ci.pre-rstor.com" = {
+          hostname = "172.30.23.11";
+        };
+        "storage-ops storage-ops-usc.packetfabric.net" = {
+          hostname = "storage-ops-usc.packetfabric.net";
+          dynamicForwards = [ { port = 1080; } ];
+          extraOptions = { ControlPersist = "12h"; };
+        };
+        "lax01-jumphost01 jump01.lax01 jump01.lax01.rstor.net 10.3.204.168" = {
+          hostname = "10.3.204.168";
+        };
+        "dca02-jumphost01 jump01.dca02 jump01.dca02.rstor.net 10.4.204.215" = {
+          hostname = "10.4.204.215";
+        };
+      };
+      extraConfig = ''
+CanonicalizeHostname yes
+CanonicalDomains rstor.net rstorcloud.io
+CanonicalizeMaxDots 1
+CanonicalizeFallbackLocal yes
+CanonicalizePermittedCNAMEs *.rstor.net:*.rstor.net *.rstorcloud.io:*.rstorcloud.io'';
+    };
   };
 }
