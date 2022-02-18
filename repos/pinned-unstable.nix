80350f37b0e4a4307ccdbf126b449a4ff17294ba
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2022-02-18 07:33:34 +0100

    Bumped pinned-unstable to get the latest discord

--- repos/pinned-unstable.nix
+++ repos/pinned-unstable.nix
@@ -1,1 +1,1 @@
-import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/69a2c62cf7f9f6e23d0e72f960fd6f902779500b.tar.gz") { }
+import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/e0c8e584ae000051db50c70cf6882e12e91b5e9f.tar.gz") { }
