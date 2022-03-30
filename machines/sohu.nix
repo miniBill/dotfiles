0b17806aacf5975457a7bf03f1b1f3e5dc4a483e
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2022-03-30 19:21:46 +0200

    New machine: sohu

---
+++ machines/sohu.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, ... }:
+
+let
+  pinned-unstable-calibre = import ../repos/pinned-unstable-calibre.nix;
+  pinned-unstable-youtube-dl = import ../repos/pinned-unstable-youtube-dl.nix;
+  pinned-unstable-discord = import ../repos/pinned-unstable-discord.nix;
+  pinned-unstable-tdesktop = import ../repos/pinned-unstable-tdesktop.nix;
+in
+{
+  imports = [ ../groups/graphical.nix ];
+
+  home.packages = with pkgs; [
+    pinned-unstable-calibre.calibre
+    libreoffice-fresh
+
+    # NET
+    pinned-unstable-youtube-dl.youtubeDL
+
+    # Im
+    pinned-unstable-discord.discord
+    pinned-unstable-tdesktop.tdesktop
+  ];
+}
