030599679ed04ba02069611623662c38524c6638
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2022-04-02 23:14:51 +0200

    Fix sohu hardware config

--- sohu/sohu-hardware.nix
+++ sohu/sohu-hardware.nix
@@ -8,17 +8,24 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "usbhid" ];
-  boot.initrd.kernelModules = [ ];
+  boot.initrd.availableKernelModules = [ "usb_storage" "usbhid" ];
+  boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
+    { device = "/dev/disk/by-uuid/e3ea59b0-29eb-48e2-905b-a7e96af07bcc";
       fsType = "ext4";
     };
 
-  swapDevices = [ ];
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/F44C-6477";
+      fsType = "vfat";
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/f8439bc9-c7e2-4c9f-b19d-fb2ce6464bce"; }
+    ];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
 }
