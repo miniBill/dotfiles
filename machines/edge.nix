bc43977fc7449d8a5022aa5da06467f3068961d7
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2022-05-20 22:53:32 +0200

    Remove duplicate software

--- machines/edge.nix
+++ machines/edge.nix
@@ -12,31 +12,28 @@ in {
 
   home.packages = with pkgs; [
     # BASE
-    exa bc patchelf file inotify-tools (aspellWithDicts (d: [d.it])) nix-bundle
+   inotify-tools (aspellWithDicts (d: [d.it])) nix-bundle
 
     # DEV
-    colordiff git gcc omnisharp-roslyn gnumake ghc
+    colordiff gcc omnisharp-roslyn gnumake ghc
     (with dotnetCorePackages; combinePackages [ sdk_3_1 ])
     dhall-lsp-server dhall
     # Elm
     elmPackages.elm-format elmPackages.elm elmPackages.elm-test
-    glslang
-    # binutils zig colordiff sqlitebrowser jre nasm dotnet-sdk
-    adoptopenjdk-jre-openj9-bin-8 # adoptopenjdk-bin
+    glslang adoptopenjdk-jre-openj9-bin-8
     qtcreator cmake clang-tools cppcheck gdb linuxPackages.perf hotspot
     rustc cargo optipng yarn
 
     # MULTIMEDIA
-    gimp spotify inkscape okular ghostscript blender glxinfo vlc imagemagick ffmpeg mediainfo qjackctl gnome3.cheese audacity
+    ghostscript blender glxinfo mediainfo qjackctl gnome3.cheese
 
     # NET
-    filezilla skypeforlinux nmap ncat bind whois pinned-unstable-youtube-dl.youtubeDL
+    skypeforlinux bind pinned-unstable-youtube-dl.youtubeDL
     (keepass.override { plugins = [ keepass-keepassrpc ]; })
-    pinned-unstable-discord.discord firefox teams aria
-    zotero
+    pinned-unstable-discord.discord teams zotero
 
     # BASE-GUI
-    ark yakuake kcharselect kolourpaint pinned-unstable-tdesktop.tdesktop kcalc gnome3.libgnomekbd spectacle wineWowPackages.stable okular gwenview
+    ark yakuake kcharselect kolourpaint pinned-unstable-tdesktop.tdesktop kcalc gnome3.libgnomekbd spectacle wineWowPackages.stable gwenview
     fira-code fira-code-symbols gparted winetricks wineWowPackages.stable libreoffice-fresh
 
     # GAMES
