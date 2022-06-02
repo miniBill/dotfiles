e322f1ac00baa16c14d2cbf9e82c19734ddbcea1
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2022-06-02 17:53:31 +0200

    Update zoom

--- repos/pinned-unstable-zoom.nix
+++ repos/pinned-unstable-zoom.nix
@@ -1,1 +1,1 @@
-import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/e0c8e584ae000051db50c70cf6882e12e91b5e9f.tar.gz") { }
+import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/fa585a07f65f4ee954758c67db6c75463a6f6560.tar.gz") { }
