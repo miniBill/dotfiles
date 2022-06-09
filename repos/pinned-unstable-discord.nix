eab87865d19591bc91f0b9bc324ef82c777e3ffc
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2022-06-09 15:30:55 +0200

    Discord 0.18

--- repos/pinned-unstable-discord.nix
+++ repos/pinned-unstable-discord.nix
@@ -1,1 +1,1 @@
-import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/e0c8e584ae000051db50c70cf6882e12e91b5e9f.tar.gz") { }
+import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/808aad6ceec1647edc14d1a8f901b9cf7a6fda17.tar.gz") { }
