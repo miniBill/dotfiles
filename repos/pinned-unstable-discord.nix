24ec278fd1e84df14e4b8a34f0a4ddaf69c7a00e
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2022-09-16 14:06:53 +0200

    Switch to unstable discord

--- repos/pinned-unstable-discord.nix
+++ repos/pinned-unstable-discord.nix
@@ -1,1 +1,1 @@
-import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/808aad6ceec1647edc14d1a8f901b9cf7a6fda17.tar.gz") { }
+import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/3fcf17c3484955b73d2e92efd3ab0da3176e02b1.tar.gz") { }
