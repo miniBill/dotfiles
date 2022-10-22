ba46b6c1368c9a74a97d3fe0bac7eef738b74c47
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2022-10-22 17:32:16 +0200

    Update Telegram Desktop

--- repos/pinned-unstable-tdesktop.nix
+++ repos/pinned-unstable-tdesktop.nix
@@ -1,1 +1,1 @@
-import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/c31421937897d65277c40b952b4577dce5f986cd.tar.gz") { }
+import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/910b2be5ac08264311123d3add6d81e8e6fd05b8.tar.gz") { }
