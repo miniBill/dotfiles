f1ffc5da0718ca267ab43fa176acfe0efbd3e419
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2022-11-01 17:52:49 +0100

    Openssl bugfix

--- repos/pinned-unstable-discord.nix
+++ repos/pinned-unstable-discord.nix
@@ -1,1 +1,1 @@
-import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/32566159ea9407c9eed9281ad8f9cd228d5cbd35.tar.gz") { }
+import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/eeca5969b3f42ac943639aaec503816f053e5e53.tar.gz") { }
