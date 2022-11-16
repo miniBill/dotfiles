7bd87cace3d2f0fe65ae1798d43c8b16c1770e1d
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2022-11-16 13:08:40 +0100

    Switching to unstable piper

--- machines/uriel.nix
+++ machines/uriel.nix
@@ -4,6 +4,7 @@ let
   pinned-unstable-calibre = import ../repos/pinned-unstable-calibre.nix;
   pinned-unstable-youtube-dl = import ../repos/pinned-unstable-youtube-dl.nix;
   # pinned-unstable-discord = import ../repos/pinned-unstable-discord.nix;
+  pinned-unstable-piper = import ../repos/pinned-unstable-piper.nix;
   # pinned-unstable-tdesktop = import ../repos/pinned-unstable-tdesktop.nix;
   maybe-qtcreator = import ../repos/maybe-qtcreator.nix;
 in
@@ -16,7 +17,7 @@ in
     # BASE
     openrgb
     (texlive.combine { inherit (texlive) academicons arydshln fontawesome5 footmisc moderncv multirow relsize scheme-medium textpos; })
-    piper
+    pinned-unstable-piper.piper
 
     # GUI
     libgnomekbd
---
+++ repos/pinned-unstable-piper.nix
@@ -0,0 +1,1 @@
+import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/d92383b18de4ec74807e740054ff00e2a3b8bcd9.tar.gz") { }
