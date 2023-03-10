161a522f533ac34d71081f7e783fc19a07a68dc7
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-03-10 20:49:43 +0100

    Use raw sha256 hashes as per source

--- programs/lamdera.nix
+++ programs/lamdera.nix
@@ -5,10 +5,10 @@ let
   arch = if stdenv.isAarch64 then "arm64" else "x86_64";
   hashes =
     {
-      "x86_64-linux" = "08h5xq6l2zdivgbyxfiivdpyifz684s1kzhb29hajvrnhws7cfj4";
-      "aarch64-linux" = "0vcq5r47w7lzjzzdaal839krw6bhanphf7lgiaaw1w2d14xyq6zi";
-      "x86_64-darwin" = "0lqqz5nqb6s473ivj6wnjlydbly4kr51mgb4pz0xn3r47v8qhnnh";
-      "aarch64-darwin" = "02dh8nzfk21ip9ar92zj6l0ryy05n1mcbl9da2xa97jv7bm9nvy6";
+      "x86_64-linux" = "443a763487366fa960120bfe193441e6bbe86fdb31baeed7dbb17d410dee0522";
+      "aarch64-linux" = "f11bec3b094df0c0958a8f1e07af5570199e671a882ad5fe979f1e7e482e986d";
+      "x86_64-darwin" = "d05a88d13e240fdbc1bf64bd1a4a9ec4d3d53c95961bb9e338449b856df91853";
+      "aarch64-darwin" = "bb105e7aebae3c637b761017c6fb49d9696eba1022f27ec594aac9c2dbffd907";
     };
 in
 
@@ -16,7 +16,6 @@ stdenv.mkDerivation rec {
   name = "lamdera-${version}";
 
   version = "1.1.0";
-
 
   src = fetchurl {
     url = "https://static.lamdera.com/bin/lamdera-${version}-${os}-${arch}";
