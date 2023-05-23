de542acf1c53c4b24f564a87b5420aa0a195b847
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-05-24 00:53:11 +0200

    This is not needed anymore

--- groups/steam.nix
+++ groups/steam.nix
@@ -2,7 +2,6 @@
 
 {
   # 32-bit support for Steam
-  hardware.opengl.driSupport32Bit = true;
   hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
   programs.steam.enable = true;
   nixpkgs.config.allowUnfreePredicate = pkg:
