01f03b612614566006f01aa4f4241a86f8ee3e57
Leonardo Taglialegne <leonardo.taglialegne@vendr.com>
2023-05-23 19:56:54 +0200

    Switch to NixOS 23.05

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "Home Manager configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-22.11";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
