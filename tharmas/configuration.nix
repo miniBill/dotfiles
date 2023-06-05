d1ac15b7830ca2054e152c67b05650ae8d153210
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-06-05 18:29:30 +0200

    Use grub only

--- tharmas/configuration.nix
+++ tharmas/configuration.nix
@@ -23,7 +23,7 @@ in
     }];
   };
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.enable = false;
 
   networking.hostName = "tharmas";
 
