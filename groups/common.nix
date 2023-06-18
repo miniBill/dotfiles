b255f7f6417ac2ba2d9de02591d7f45f443c40b3
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-06-18 23:12:50 +0200

    Don't wait for online

--- groups/common.nix
+++ groups/common.nix
@@ -24,6 +24,7 @@
   networking.firewall.allowedUDPPorts = [ config.services.tailscale.port ];
   # Strict reverse path check can break tailscale exit nodes
   networking.firewall.checkReversePath = "loose";
+  systemd.services.NetworkManager-wait-online.enable = false;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
