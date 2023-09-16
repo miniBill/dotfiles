2a0b1595b3f080b2f4afc1a7c27fd0762a98e787
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-09-16 15:19:51 +0200

    Thamiel was installed as 23.05

--- thamiel/configuration.nix
+++ thamiel/configuration.nix
@@ -13,4 +13,6 @@
   users.users.root.openssh.authorizedKeys.keys = [
     ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINGAVCUqG9wVONKAUB449Zn+B/6nbKPFOlCcyCC55u3K minibill@uriel''
   ];
+
+  system.stateVersion = "23.05";
 }
