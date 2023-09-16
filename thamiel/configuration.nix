e156aaefddfdb04c16d6f08fb2c64f7fa61993e8
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-09-16 15:18:48 +0200

    Apply standard server configuration to thamiel

--- thamiel/configuration.nix
+++ thamiel/configuration.nix
@@ -1,13 +1,15 @@
 { ... }: {
   imports = [
     ./hardware-configuration.nix
+
+    ../groups/common.nix
+    ../groups/server.nix
   ];
 
-  boot.cleanTmpDir = true;
+  networking.hostName = "thamiel";
+
   zramSwap.enable = true;
-  networking.hostName = "vps-b32a4601";
-  networking.domain = "vps.ovh.net";
-  services.openssh.enable = true;
+
   users.users.root.openssh.authorizedKeys.keys = [
     ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINGAVCUqG9wVONKAUB449Zn+B/6nbKPFOlCcyCC55u3K minibill@uriel''
   ];
