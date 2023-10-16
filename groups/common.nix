0cefe6e3c3b54fbb5fb158ff6035b92522a297f5
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-10-16 04:46:02 +0200

    Harden ssh config

--- groups/common.nix
+++ groups/common.nix
@@ -60,7 +60,41 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   programs.mtr.enable = true;
-  programs.ssh.startAgent = true;
+  programs.ssh = {
+    startAgent = true;
+
+    ciphers = [
+      "chacha20-poly1305@openssh.com"
+      "aes256-gcm@openssh.com"
+      "aes128-gcm@openssh.com"
+      "aes256-ctr"
+      "aes192-ctr"
+      "aes128-ctr"
+    ];
+    kexAlgorithms = [
+      "sntrup761x25519-sha512@openssh.com"
+      "curve25519-sha256"
+      "curve25519-sha256@libssh.org"
+      "diffie-hellman-group16-sha512"
+      "diffie-hellman-group18-sha512"
+      "diffie-hellman-group-exchange-sha256"
+    ];
+    macs = [
+      "hmac-sha2-256-etm@openssh.com"
+      "hmac-sha2-512-etm@openssh.com"
+      "umac-128-etm@openssh.com"
+    ];
+    hostKeyAlgorithms = [
+      "sk-ssh-ed25519-cert-v01@openssh.com"
+      "ssh-ed25519-cert-v01@openssh.com"
+      "sk-ssh-ed25519@openssh.com"
+      "ssh-ed25519"
+      "rsa-sha2-512-cert-v01@openssh.com"
+      "rsa-sha2-256-cert-v01@openssh.com"
+      "rsa-sha2-512"
+      "rsa-sha2-256"
+    ];
+  };
   programs.zsh = {
     enable = true;
     enableCompletion = true;
