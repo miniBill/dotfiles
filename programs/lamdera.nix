f28dbcd92a6a3582e66d33efc4776841e59bc84d
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-12-27 13:56:00 +0100

    Use lamdera-next

--- programs/lamdera.nix
+++ programs/lamdera.nix
@@ -8,7 +8,7 @@ let
   arch = if stdenv.isAarch64 then "arm64" else "x86_64";
   hashes =
     {
-      "x86_64-linux" = "b13110bacc3f71c2a3e12c52172a821a85cc13243a95249ca18c8beb296c0ce8";
+      "x86_64-linux" = "YI9p52SeCDORHZIntUZRwt0/jh+ZfW0C1iQbw6hTlzA=";
       "aarch64-linux" = "afbc71f0570b86215942d1b4207fe3de0299e6fdfd2e6caac78bf688c81b9bd1";
       "x86_64-darwin" = "50a3df09b02b34e1653beb1507c6de0f332674e088ded7c66af4e5987753304e";
       "aarch64-darwin" = "174a5bfec355361c4f030861405513818be25fd7e4325f7221aa71ebd27475d3";
@@ -17,10 +17,10 @@ in
 
 stdenv.mkDerivation rec {
   pname = "lamdera";
-  version = "1.2.0";
+  version = "next";
 
   src = fetchurl {
-    url = "https://static.lamdera.com/bin/lamdera-${version}-${os}-${arch}";
+    url = "https://static.lamdera.com/bin/next/lamdera-${version}-${os}-${arch}";
     sha256 = hashes.${stdenv.system};
   };
 
