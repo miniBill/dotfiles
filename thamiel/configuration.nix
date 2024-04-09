9dba612e3ba3ae56199c57a0809e1e05d7e96326
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2024-04-09 13:24:53 +0200

    Thamiel is the NAS

--- thamiel/configuration.nix
+++ thamiel/configuration.nix
@@ -1,30 +1,18 @@
-{ ... }: {
+{ config, lib, pkgs, ... }:
+
+{
   imports = [
     ./hardware-configuration.nix
-
     ../groups/common.nix
     ../groups/server.nix
   ];
 
+  boot.loader.grub.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  fileSystems."/".options = [ "noatime" "nodiratime" "discard" ];
+
   networking.hostName = "thamiel";
 
-  zramSwap.enable = true;
-  services.logind.extraConfig = "RuntimeDirectorySize=500M";
-
-  users.mutableUsers = false;
-  users.users = {
-    minibill = {
-      isNormalUser = true;
-      extraGroups = [ "wheel" ];
-      hashedPassword = "$6$dPi0t.m3Rat601ku$zULH0TmfZQPZzxnQEchMGKLEUxnEFYkT47zWnNTfm3yopOEo5CdD6Ymhr1yIakq5zwtIaXDCAoaJWNYm5My0W0";
-    };
-    root = {
-      hashedPassword = "$6$fWJ47Jp5U7LytfoV$Z1XqNGZIA0m9MUtNgmmjIGqsKkyoqT0PhQ0F7OyMrElwtjHeRrDUu5PzISxuUXgcxIauyA/8R/IH7r7cWq4Fu/";
-      openssh.authorizedKeys.keys = [
-        ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINGAVCUqG9wVONKAUB449Zn+B/6nbKPFOlCcyCC55u3K minibill@uriel''
-      ];
-    };
-  };
-
-  system.stateVersion = "23.05";
+  system.stateVersion = "23.11";
 }
