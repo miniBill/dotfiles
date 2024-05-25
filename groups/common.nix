01d6837ad44f2b9063ffd7eb493ccad79f0b99b1
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2024-05-25 20:45:48 +0200

    Add cachix keys

--- groups/common.nix
+++ groups/common.nix
@@ -158,6 +158,9 @@
     settings = {
       auto-optimise-store = true;
       experimental-features = [ "nix-command" "flakes" ];
+
+      extra-substituters = "https://devenv.cachix.org";
+      extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
     };
     package = pkgs.nixFlakes;
   };
