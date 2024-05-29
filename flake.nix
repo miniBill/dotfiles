b9fd142e58ee8fb6bef46c4414629f23d0256610
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2024-05-30 01:31:54 +0200

    Update to 24.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     musnix = {
       url = "github:musnix/musnix";
       inputs.nixpkgs.follows = "nixpkgs";
