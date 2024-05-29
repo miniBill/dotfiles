1db9cc1b508bfb118bd4588d37648d8c9e929ecd
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2024-05-30 01:43:30 +0200

    Update to nixos 24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Home Manager configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
 
     lamdera = {
       url = "github:miniBill/lamdera-flake";
@@ -11,7 +11,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
