ea5e10600bf90d72eea6eabee1425b4edb814840
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2024-12-23 21:48:36 +0100

    Flakes now in stable

--- groups/machines/common.nix
+++ groups/machines/common.nix
@@ -177,7 +177,6 @@
       extra-substituters = "https://devenv.cachix.org";
       extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
     };
-    package = pkgs.nixFlakes;
   };
 
   # Users
