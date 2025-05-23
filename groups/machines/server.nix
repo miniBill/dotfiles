0090da69cb7abcc9de0a778bac3b2ce1201bc1e4
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2025-05-23 18:18:47 +0200

    Fix locales on server (?)

--- groups/machines/server.nix
+++ groups/machines/server.nix
@@ -5,14 +5,17 @@ _:
   i18n = {
     defaultLocale = "en_US.UTF-8";
     extraLocales = [
-      "en_GB.UTF-8"
-      "en_US.UTF-8"
+      "en_GB.UTF-8/UTF-8"
+      "en_US.UTF-8/UTF-8"
     ];
   };
   time.timeZone = "UTC";
 
   services.openssh = {
-    ports = [ 22 2222 ];
+    ports = [
+      22
+      2222
+    ];
   };
 
   services.cron.enable = true;
