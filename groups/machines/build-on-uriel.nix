6960512c0d362c472d347bc99396e7956bf93fbf
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2025-06-27 13:11:14 +0200

    Remove redundant comment

--- groups/machines/build-on-uriel.nix
+++ groups/machines/build-on-uriel.nix
@@ -1,9 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  # You can see the resulting builder-strings of this NixOS-configuration with "cat /etc/nix/machines".
-  # These builder-strings are used by the Nix terminal tool, e.g.
-  # when calling "nix build ...".
   nix.buildMachines = [
     {
       hostName = "uriel";
