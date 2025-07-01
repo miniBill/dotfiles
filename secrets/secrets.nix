69511c1df75ba31f87634b600e197af71e20ca94
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2025-07-01 12:41:10 +0200

    Add nathanda's key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -5,6 +5,7 @@ let
   edge = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMT/em7DhFIVRdXQ+zRWneD5t3T9pk3OXFVQ7NAP+bjB root@edge";
   ithaca = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID+WUwSkJFEgHsHazSafR5czJiZN62oCm/ox8X2ViQ47 root@ithaca";
   milky = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICDSS99OTEcGWMtfdX+XU3JotWiBKtDpTwJ16FzZOVeZ root@milky";
+  nathanda = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL7Zqky4160wkC79OABmxQkwUPli5+8zXIUw/dd5VseU root@nathanda";
   sohu = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILG+7Xahq/EMv+L+weICiUNgnU5cxb9owEhyoGG8nVaZ root@sohu";
   thamiel = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPQGYonsRdGY68K4KiawiCOn/lWGGqUX8iLHvfRKtML1 root@thamiel";
   tharmas = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF8UP9vkY+7OROsHeqZCnBlOwwPZW5fdoyENlxfpIGPl root@tharmas";
@@ -12,6 +13,7 @@ let
 
   home = [
     edge
+    nathanda
     sohu
     uriel
   ];
