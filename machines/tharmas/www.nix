bee81162c3921cc14ed3c9523d9dd006a8bd4777
Leonardo Taglialegne <leonardo@taglialegne.it>
2025-12-22 21:41:10 +0100

    Fix admin path matching

--- machines/tharmas/www.nix
+++ machines/tharmas/www.nix
@@ -156,7 +156,7 @@ in
             try_files $uri $uri/ /index.php$is_args$args;
           }
 
-          location /(admin[a-z0-9]*)/ {
+          location ~ /(admin[a-z0-9]*)/ {
             try_files $uri $uri/ /$1/index.php$is_args$args;
           }
 
