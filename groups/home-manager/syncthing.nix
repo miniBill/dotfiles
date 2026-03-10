ef224bd714c2d6535f9a6fa7318b8ad7f5a48e1b
Leonardo Taglialegne <leonardo@taglialegne.it>
2026-03-10 15:54:59 +0100

    Add tharmas to syncthing config

--- groups/home-manager/syncthing.nix
+++ groups/home-manager/syncthing.nix
@@ -8,6 +8,7 @@
         "malia".id = "TFSZUCL-6NK3K6Q-V6VAWRB-IHFFTOE-SCDL3ZJ-DGF37N5-6MLVIUG-PX2BWAF";
         "nathanda".id = "JOM45EK-UVQ7OMB-EFCTKCN-5SMMNE7-YO22NCM-4PSQ3CK-QBLPVOW-EDVV4QK";
         "uriel".id = "QPKB44L-E7E7WID-TSB6EWI-6J7IUEX-BGRVH3A-ZVTXZ7B-WQZ2SJE-TGDAXAR";
+        "tharmas".id = "RDWCC6T-OM7ZIT2-UL2JUFF-CVO7I6F-6TUL5XW-FPX4PJE-FYMDXCG-Q5HD7QZ";
       };
 
       folders = {
@@ -16,6 +17,7 @@
             "uriel"
             "malia"
             "nathanda"
+            "tharmas"
           ];
           path = "~/Sync/Global";
           versioning = {
@@ -30,6 +32,7 @@
           devices = [
             "uriel"
             "nathanda"
+            "tharmas"
           ];
           path = "~/Sync/Graphical";
           versioning = {
