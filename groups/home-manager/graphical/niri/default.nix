62ceaf9d5e0898681581b6e2f8c4ce87ddb2f897
Leonardo Taglialegne <leonardo@taglialegne.it>
2026-07-22 12:24:31 +0200

    Switch to hyprlock for better fingerprint support and nicer UI

--- groups/home-manager/graphical/niri/default.nix
+++ groups/home-manager/graphical/niri/default.nix
@@ -26,8 +26,74 @@ in
   programs = {
     alacritty.enable = true; # Super+T in the default setting (terminal)
     fuzzel.enable = true; # Super+D in the default setting (app launcher)
-    swaylock.enable = true; # Super+Alt+L in the default setting (screen locker)
   };
+
+  programs.hyprlock.enable = true;
+  programs.hyprlock.settings = {
+    general.hide_cursor = false;
+
+    auth.fingerprint = {
+      enabled = true;
+      retry_delay = 250; # in milliseconds
+    };
+
+    background.path = "~/Pictures/Wallpapers/Witch.jpg";
+
+    input-field = {
+      size = "20%, 5%";
+      outline_thickness = "3";
+      inner_color = "rgba(0, 0, 0, 0.0) # no fill";
+
+      outer_color = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+      check_color = "rgba(00ff99ee) rgba(ff6633ee) 120deg";
+      fail_color = "rgba(ff6633ee) rgba(ff0066ee) 40deg";
+
+      font_color = "rgb(143, 143, 143)";
+      fade_on_empty = "false";
+      rounding = "15";
+
+      placeholder_text = "Input password...";
+      fail_text = "$PAMFAIL$FPRINTFAIL";
+
+      # uncomment if you wish to display a message during authentication;
+      check_text = "Authenticating...";
+
+      # uncomment to use a letter instead of a dot to indicate the typed password;
+      # dots_text_format = "*";
+      # dots_size = 0.4;
+      dots_spacing = "0.3";
+
+      # uncomment to use an input indicator that does not show the password length (similar to swaylock's input indicator);
+      # hide_input = true;
+
+      # position = "0, -20";
+      halign = "center";
+      valign = "center";
+    };
+
+    label = [
+      #  TIME
+      {
+        text = "$TIME"; # ref. https://wiki.hyprland.org/Hypr-Ecosystem/hyprlock/#variable-substitution
+        font_size = 90;
+
+        position = "-30, 0";
+        halign = "right";
+        valign = "top";
+      }
+
+      #  DATE
+      {
+        text = "cmd[update:60000] date +\"%A, %d %B %Y\""; # update every 60 seconds
+        font_size = 25;
+
+        position = "-30, -150";
+        halign = "right";
+        valign = "top";
+      }
+    ];
+  };
+
   services = {
     mako.enable = true; # notification daemon
     swayidle.enable = true; # idle management daemon
